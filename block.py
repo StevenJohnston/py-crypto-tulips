701e3f247732258942661eb615e45d5ac8cfdd32
Matt Warren <warren.matt@hotmail.com>
2018-02-12 17:07:17 -0500

    small redis changes

--- block.py
+++ block.py
@@ -1,13 +1,22 @@
-# Stored in database with keys:
-#       block:id:hash 
-#   and 
-#       block:id:data
+import redis
+
+# Stored in database with key -> value
+#       block:id:actual_hash_here -> block_data
 
 class Block:
     __block_hash = '' 
     __block_data = ''
 
+    key_format =  'block:{}:{}'
+
+    id_store_key = 'block:current_id'
 
     def __init__(self, block_hash, block_data):
         self.block_hash = block_hash
         self.block_data = block_data
+
+
+    def store_block(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        r.get(Block.id_store_key)
+        r.set(Block.key_format.format(id, self.block_hash), self.block_data)
