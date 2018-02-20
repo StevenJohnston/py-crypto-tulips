53348e0eae389d47647f2b8146caf08fe2c0cc21
Matt <warren.matt@hotmail.com>
2018-02-19 20:26:37 -0500

    - added hash_service for transactions and contracts - removed transaction_service (hash_service replaces this)

--- CryptoTulip/dal/block/block.py
+++ CryptoTulip/dal/block/block.py
@@ -2,12 +2,16 @@
 #       block:id:actual_hash_here -> block_data
 
 class Block:
-    __block_hash = ''
-    __block_data = ''
+    block_hash = ''
+    block_data = ''
+    prefix = 'block'
 
     def __init__(self, block_hash, block_data):
         self.block_hash = block_hash
         self.block_data = block_data
 
     def to_string(self):
-        return self.block_hash + "|" + self.block_data
+        return str(self.block_hash) + "->" + str(self.block_data)
+
+    def from_json(self, json_str):
+        self.block_hash = ""
