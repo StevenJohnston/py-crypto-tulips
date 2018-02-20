469aa1091a6926ea5be0f2419f1dac6ea7fc3d73
WilliamPring <mrwilliampring@gmail.com>
2018-02-20 13:07:28 -0500

    task/CRYP-6 commiting the finish version of Hashing and its testing will add enhance more if needed

---
+++ crypto-tulip/hashing/crypt_hashing.py
@@ -0,0 +1,24 @@
+"""
+Hashing Modular
+"""
+
+import hashlib
+import json
+
+
+class Hashing:
+    """
+    The Hashing Class that has static methods to help with different type of hashing
+    """
+
+    @staticmethod
+    def hashing_block(json_block):
+        """ Generates and returns a sha256 hash
+
+        Keyword arugments:
+        json_block -- JSON string
+
+        Returns:
+        logger -- Returns the sha256 hash of the JSON string provided
+        """
+        return hashlib.sha256((json.dumps(json_block)).encode('utf-8')).hexdigest()
