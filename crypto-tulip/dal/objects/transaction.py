40f05e56c84825b980a3b24d48f0c836cf29450d
Matt <warren.matt@hotmail.com>
2018-02-21 19:20:27 -0500

    - cleaned up hash service a lot - tests for hash service - moved config files to config folder - added logs foder - adjusted paths to appropriate folders

--- crypto-tulip/dal/objects/transaction.py
+++ crypto-tulip/dal/objects/transaction.py
@@ -1,5 +1,9 @@
-# Stored in database as a hash with key:
-#       transaction:hash
+"""
+Transaction Class
+"""
+
+import json
+import time
 
 class Transaction:
 
@@ -11,13 +15,25 @@ class Transaction:
     amount = ''
     timestamp = ''
 
-    def __init__(self, transaction_hash, to_addr, from_addr, amount, timestamp):
+    def __init__(self, transaction_hash, to_addr, from_addr, amount, timestamp = time.strftime("%Y/%m/%d-%H:%M:%S")):
         self.transaction_hash = transaction_hash
         self.to_addr = to_addr
         self.from_addr = from_addr
-        self.amount = amount
+        self.amount = float(amount)
         self.timestamp = timestamp
 
+    @staticmethod
+    def from_dict(dict_values):
+        transaction_hash = dict_values.get('transaction_hash')
+        to_addr = dict_values.get('to_addr')
+        from_addr = dict_values.get('from_addr')
+        amount = dict_values.get('amount')
+        timestamp = dict_values.get('timestamp')
+        new_transaction = Transaction(transaction_hash, to_addr, from_addr, amount, timestamp)
+        return new_transaction
 
-    def _from_json(self, json_str):
-        self.transaction_hash = ""
+    def to_string(self):
+        return json.dumps(self.__dict__)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
