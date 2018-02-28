98f685d4c341a69dd6c3f3b4916a57016bb61028
WilliamPring <mrwilliampring@gmail.com>
2018-02-28 14:36:27 -0500

    Commiting the strucuture of the class

---
+++ services/transaction_service/transaction_service.py
@@ -0,0 +1,30 @@
+"""
+Transaction Service Module
+"""
+
+
+class TransactionService:
+    """
+    Transaction Service
+    """
+
+    @staticmethod
+    def create_new_transaction():
+        """
+        Method Comment
+        """
+        pass
+
+    @staticmethod
+    def verfiy_rsa():
+        """
+        Method Comment
+        """
+        pass
+
+    @staticmethod
+    def verify_ownership_of_funds():
+        """
+        Method Comment
+        """
+        pass
