0672e4bf004a5c6dff8360cc8c62e240a1f822f2
Steven Johnston <me@stevenjohnston.ca>
2018-03-02 19:49:45 -0500

    subtask/CRYP-91. Working child process listening to bitfinex websocket. Still requires redis storing

--- crypto_tulips/market/bitfinex.py
+++ crypto_tulips/market/bitfinex.py
@@ -1,17 +1,11 @@
 from .exchange import Exchange
 from crypto_tulips.dal.objects.price_stamp import PriceStamp
-<<<<<<< HEAD
 from crypto_tulips.dal.services.hash_service import HashService
-=======
->>>>>>> a27cfd09d3316388877cba22ddf6a9c0288c60e8
 import json
 
 
 class Bitfinex(Exchange):
-<<<<<<< HEAD
-=======
 
->>>>>>> a27cfd09d3316388877cba22ddf6a9c0288c60e8
     WEBSOCKET_URL = "wss://api.bitfinex.com/ws/2"
     REST_URL = None
     SUBSCRIBE_TRADES = json.dumps({ 
@@ -28,26 +22,16 @@ class Bitfinex(Exchange):
     @classmethod
     def on_message(self, ws, message):
         msg = json.loads(message)
-<<<<<<< HEAD
         print(msg)
         if isinstance(msg, list) :
             if msg[1] == "tu":
                 # add this to the db
                 hashService = HashService()
                 #hashService.store_hash(self.trade_pricestamp_adaptor(msg))
-=======
-        if msg[1] == "tu":
-            # add this to the db
-            self.trade_pricestamp_adaptor(msg)
->>>>>>> a27cfd09d3316388877cba22ddf6a9c0288c60e8
         
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     bitfinex = Bitfinex()
     bitfinex.start()
-=======
-    Bitfinex()
->>>>>>> a27cfd09d3316388877cba22ddf6a9c0288c60e8
   
