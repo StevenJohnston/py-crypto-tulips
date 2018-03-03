38900aa44bcd875b27b9eea69e5c2ebbf02e6e74
WilliamPring <mrwilliampring@gmail.com>
2018-03-03 15:00:33 -0500

    Commiting the wrapper function and its testing

--- crypto_tulips/hashing/crypt_hashing.py
+++ crypto_tulips/hashing/crypt_hashing.py
@@ -4,6 +4,9 @@ Hashing Module
 
 import hashlib
 import json
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
 
 
 class Hashing:
@@ -22,3 +25,102 @@ class Hashing:
         string -- Returns the sha256 hash of the JSON string provided
         """
         return hashlib.sha256((json.dumps(json_block)).encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def hashing_transaction(transaction_format):
+        """ Return a sha256 base on a string
+
+        Keyword arugments:
+        transaction_format -- a string that should be formated with pipes
+
+        Returns:
+        string -- Returns the sha256 hash of base on the string provided
+        """
+        return hashlib.sha256(transaction_format.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def generate_rsa_key(secret):
+        """ Generates a public and private key
+
+        Keyword arugments:
+        secret -- passphrase
+
+        Returns:
+        pub_key -- public key
+        key -- private key
+        """
+        key = RSA.generate(2048)
+        key.exportKey(passphrase=secret, pkcs=8, protection="scryptAndAES128-CBC")
+        pub_key = key.publickey()
+        return pub_key.exportKey().decode(), key.exportKey().decode()
+
+    @staticmethod
+    def encode_signature_of_data(data, private_key):
+        """ A wrapper function that return a signiture base on the private key and data
+        but you can provide a string for this function
+
+        Keyword arugments:
+        data -- data that being pass to be signed
+        private_key -- Private Key
+
+        Returns:
+        signature_of_data() -- the signiture of the sign data
+        """
+        data_as_bytes = str.encode(data)
+        return Hashing.signature_of_data(data_as_bytes, private_key)
+
+
+    @staticmethod
+    def signature_of_data(data, private_key):
+        """ Returns a signiture base on the private key and data
+
+        Keyword arugments:
+        data -- data that being pass to be signed
+        private_key -- Private Key
+
+        Returns:
+        signature -- the signiture of the sign data
+        """
+        hash_message = SHA256.new(data)
+        pkey = RSA.import_key(private_key)
+        signature = pkcs1_15.new(pkey).sign(hash_message)
+        return signature
+
+
+    def encode_validate_signature(data, pub_key, signature):
+        """ A wrapper function that Validate the signiture base on the data, public key and a signiture
+        but you can provide a string for this function
+
+        Keyword arugments:
+        data -- data that being pass to be signed
+        pub_key -- public key
+        data -- data to be validiated
+
+        Returns:
+        validate_signature() -- If the signiture is valided or not
+        """
+        data_as_bytes = str.encode(data)
+        return Hashing.validate_signature(data_as_bytes, pub_key, signature)
+
+
+    @staticmethod
+    def validate_signature(data, pub_key, signature):
+        """ Validate the signiture base on the data, public key and a signiture
+
+        Keyword arugments:
+        data -- data that being pass to be signed
+        pub_key -- public key
+        data -- data to be validiated
+
+        Returns:
+        bool -- If the signiture is valided or not
+        """
+        rsakey = RSA.importKey(pub_key)
+        hash_data = SHA256.new(data)
+        try:
+            pkcs1_15.new(rsakey).verify(hash_data, signature)
+            print("The signature is valid.")
+            return True
+        except (ValueError, TypeError):
+            print("The signature is not valid.")
+            return False
--- crypto_tulips/hashing/test_hashing.py
+++ crypto_tulips/hashing/test_hashing.py
@@ -13,7 +13,6 @@ def test_hashing_string():
     hash = hashing_string("test")
     assert hash == "4d967a30111bf29f0eba01c448b375c1629b2fed01cdfcc3aed91f1b57d5dd5e"
 
-
 def test_hashing_json():
     hash = hashing_string(jsonTest)
     assert hash == "08f04b54179fffeea74ebcef9c799562f478150d40b5abba1a8cd36ae53f2b0a"
@@ -24,3 +23,35 @@ def test_hashing_invalid_json():
 
 def hashing_string(string_to_hash):
     return crypt_hashing.Hashing.hashing_block(string_to_hash)
+
+
+def test_valid_signature():
+    message = b'To be signed'
+    a, b = crypt_hashing.Hashing.generate_rsa_key("asdfasdf")
+    c = crypt_hashing.Hashing.signature_of_data(message, b)
+    d = crypt_hashing.Hashing.validate_signature(message, a, c)
+    assert d == True
+
+def test_invalid_signature():
+    message = b'To be signed'
+    a, b = crypt_hashing.Hashing.generate_rsa_key("asdfasdf")
+    c = crypt_hashing.Hashing.signature_of_data(message, b)
+    message = b'change message'
+    d = crypt_hashing.Hashing.validate_signature(message, a, c)
+    assert d == False
+
+def test_normal_string_signature():
+    message = "asdfasdf"
+    a, b = crypt_hashing.Hashing.generate_rsa_key(message)
+    c = crypt_hashing.Hashing.encode_signature_of_data(message, b)
+    d = crypt_hashing.Hashing.encode_validate_signature(message, a, c)
+    assert d == True
+
+
+def test_invalid_string_signature():
+    message = "asdfasdf"
+    invalid = "asdf"
+    a, b = crypt_hashing.Hashing.generate_rsa_key(message)
+    c = crypt_hashing.Hashing.encode_signature_of_data(message, b)
+    d = crypt_hashing.Hashing.encode_validate_signature(invalid, a, c)
+    assert d == False
